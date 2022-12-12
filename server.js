40b2537f555c408b37f9bca7b7df6cbaedb78a54
boubacarsow <42849861+boubacarsow@users.noreply.github.com>
2022-12-12 15:13:11 -0700

    My server is set up and running

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const app = express();
 
-const fruits = ['apple', 'banana', 'pear'];
+//const fruits = ['apple', 'banana', 'pear'];
 
-app.get('/fruits', (req, res) => {
-    res.send(fruits);
+app.get('/', (req, res) => {
+    console.log('Welcome to the Pokemon App!')
+    res.send('Welcome to the Pokemon App!');
 });
 
-app.listen(3000, () => {
-    console.log('listening');
-});
+app.listen(3000)
